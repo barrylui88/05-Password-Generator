ca7b98d6f790eaec66f8b418315658d30cf4bf65
bcl <barrylui88@googlemail.com>
2023-01-15 23:58:26 -0300

    Code streamlined and all pseudocode added.

--- script.js
+++ script.js
@@ -1,112 +1,63 @@
-// Array of special characters to be included in password
+// ALL VARIABLES/ARRAYS/OBJECTS
+
+//// Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
 ];
 
-// Array of numeric characters to be included in password
+//// Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters to be included in password
+//// Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 ];
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+//// Array of uppercase characters to be included in password
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ];
 
-// Function to prompt user for password options
+//// Length of Password Variable
+//// Include Lowercase Characters Boolean
+//// Include Uppercase Characters Boolean
+//// Include Numbers Boolean
+//// Include Special Characters Boolean
+//// Character Type Selection Array
+//// Generated Password Character Array
+//// Generated Password Variable
+
+// FUNCTIONS
+
+//// Function to prompt user for password options
 function getPasswordOptions() {
-
+  //// Ask for desired length of password
+  //// Ask whether lowercase characters should be included
+  //// Ask whether uppercase characters should be included
+  //// Ask whether numbers should be included
+  //// Ask whether special characters should be included
+  //// Check whether at least one of the above is 'true'
+  //// If not, display error and ask again
+  //// Update the Character Type Selection Array
 }
 
-// Function for getting a random element from an array
+//// Function for getting a random element from an array
 function getRandom(arr) {
-
+  //// Find the length of the array.
+  //// Generate a random number and multiply it by the length of the array.
+  //// Return the random number'th' index of that array.
 }
 
-// Function to generate password with user input
+//// Function to generate password with user input
 function generatePassword() {
-
+  //// Generate a random number (range depends on which character types have been selected)
+  //// Based on the number generated, convert it to an according character of that type.
+  //// Update this as the Generated Password and return the value
 }
 
-// Get references to the #generate element
+//// Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
+//// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
@@ -114,5 +65,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+//// Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
